5eb2fc47284c010413cbc8ca1de12f402bc928f7
juan santana <72139544+juan-santana@users.noreply.github.com>
2022-07-12 21:31:51 -0500

    initial commit

---
+++ db.js
@@ -0,0 +1,23 @@
+const task = require("./data/items");
+const listas = require("./data/listas.json");
+const shoppinglist = require("./data/shoppinglist.json");
+const lunes = require("./data/lunesweek.json");
+const martes = require("./data/martesweek.json");
+const miercoles = require("./data/miercolesweek.json");
+const jueves = require ("./data/juevesweek.json");
+const viernes = require("./data/viernes.json");
+const sabados = require( "./data/sabadoweek.json");
+const domingos = require("./data/domingo.json");
+
+module.exports = {
+    task,
+    listas,
+    shoppinglist,
+    lunes,
+    martes,
+    miercoles,
+    jueves,
+    viernes,
+    sabados,
+    domingos
+}
